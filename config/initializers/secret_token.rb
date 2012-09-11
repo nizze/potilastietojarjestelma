bf196b38b3e60e4f94090495ffbd72c93d7c1e69
Nizar Jouini <nizar.jouini@gmail.com>
2012-09-11 19:40:26 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Potilasjarjestelma::Application.config.secret_token = '04b0239ad09340f12558751f2af31f6917412f4477f08f739b449c43b6d2991d04539430d901f780a3ce997a75a24c4bb6557fd38d2c40395de8b1588168a006'
